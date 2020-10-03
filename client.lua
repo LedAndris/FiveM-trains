634493f3c1e3951f11e519542d615e45be162efe
LedAndris <ledererandras2004@gmail.com>
2020-10-04 01:14:24 +0200

    Removed blips cuz it was shit and useless

--- client.lua
+++ client.lua
@@ -11,20 +11,11 @@ AddEventHandler('train:GetBack', function(info, schedule)
 TriggerServerEvent('train:Register', CreateTrain())
     end
 trains = info
-for k,v in pairs(trains) do
-    if GetBlipFromEntity(NetworkGetEntityFromNetworkId(v.engine)) == 0 then
-        print('Making new blip for joining player... (blip: '..GetBlipFromEntity(NetworkGetEntityFromNetworkId(v.engine))..')')
-makeENTBlip(1, 66, 1.0, 'Vonat', NetworkGetEntityFromNetworkId(v.engine), false, #v.carts)
-    end
-end
+
 end)
 
 
-RegisterNetEvent('ENT:Blip')
-AddEventHandler('ENT:Blip', function(sprite, color, size, title, ent, short, num)
-    print('Blip event triggered with id: '..ent)
-    blip = makeENTBlip(sprite, color, size, title, NetworkGetEntityFromNetworkId(ent), short, num)
-end)
+
 RegisterNetEvent('train:Create')
 AddEventHandler('train:Create', function()
     print('Creating train...')
@@ -45,23 +36,6 @@ RegisterCommand('trainspeed', function(source, args)
     print(GetEntitySpeed(NetToVeh(v.engine)))
     end
 end, false)
-
-AddEventHandler('onResourceStop', function(resourceName)
-    if (GetCurrentResourceName() ~= resourceName) then
-        for k,v in pairs(trains) do
-            for i,e in pairs(v) do
-            if type(e) == 'table' then
-                for ind, cart in pairs(e) do
-                    DeleteEntity(NetworkGetEntityFromNetworkId(cart))
-                end
-            else
-                DeleteEntity(NetworkGetEntityFromNetworkId(e))
-            end
-            end
-            end
-            print('Deleted all trains')
-    end
-  end)
 
   RegisterCommand('cleartrains', function()
     clear()
--- functions.lua
+++ functions.lua
@@ -69,7 +69,7 @@ function getTrainCarts(train)
 function CreateTrain()
     print('Creating train in function...')
     local location = config.spawns[math.random(1, #config.spawns)]
-    DeleteAllTrains() --doesnt fucking work
+    DeleteAllTrains()
     loadTrains()
     math.randomseed(GetGameTimer())
     local traintype = math.random(0, 23)
@@ -82,7 +82,6 @@ function CreateTrain()
     else
         print('^1Something went wrong while creating driver, id: '..ped)
     end
-    TriggerServerEvent('API:GlobalizeEvent', 'ENT:Blip', 1, 2, 1.0, 'Vonat', VehToNet(train), false, #getTrainCarts(train))
     print('Train created with id: '..train)
     SetTrainCruiseSpeed(train, config.speed)
     local object = {}
